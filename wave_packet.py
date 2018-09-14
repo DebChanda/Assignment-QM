17e1224c005446eaa366606d34e6115644fa8a16
DebChanda <Not inportant>
2018-09-14 10:30:15 +0530

    Added x-axis and classical particle position, also made the whole progrma faster

--- wave_packet.py
+++ wave_packet.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python3
 
-# Additional problem 1 of Assignment 6. (Not conplete yet)
+# Additional problem 1 of Assignment 6. (Nogit addt conplete yet)
 
 import os
 import numpy as np
@@ -9,35 +9,24 @@ import matplotlib.pyplot as plt
 os.system("clear")
 
 k0 = int(input("Enter the value of k0: "))
-#width = float(input("Enter the width of packet on both side of k0 :"))
 width = 1 # lower value will make the program faster
 k_min = k0 - width
 k_max = k0 + width
 del_k = 0.01
-gamma = .01 # To be choosed so that A is peaked
+gamma = 0.01 # To be choosed so that A is peaked
 
 k_linspace = np.linspace(k_min, k_max, int(2 * width / del_k))
 
-def amplitude(k_linspace, k0, gamma):
+def generator(k_linspace):
 
-    amplitude_linspace = []
-    for item in k_linspace:
-        a = (( item - k0) ** 2 + gamma **2 ) ** -1
-        amplitude_linspace.append(a)
-    amplitude_linspace = np.array(amplitude_linspace)
-    return amplitude_linspace
+    global k0, gamma
 
-def w(k_linspace):
+    amplitude_linspace = 1 / (( k_linspace - k0) ** 2 + gamma **2 )
+    w_linspace = k_linspace ** 2/2
 
-    w_linspace = []
-    for item in k_linspace:
-        w_linspace.append((item ** 2) / 2)
-    w_linspace = np.array(w_linspace)
-    return w_linspace
+    return amplitude_linspace, w_linspace
 
-
-amplitude_linspace = amplitude(k_linspace, k0, gamma)
-w_linspace = w(k_linspace)
+amplitude_linspace, w_linspace = generator(k_linspace)
 
 def psi(x, t):
 
@@ -45,26 +34,35 @@ def psi(x, t):
     n = int(2 * width / del_k)
     psi_xt = 0
     for i in range(n):
-        psi_xt += amplitude_linspace[i] * np.cos(-k_linspace[i] * x - w_linspace[i] * t)
+        psi_xt += del_k * amplitude_linspace[i] * np.cos(k_linspace[i] * x - w_linspace[i] * t)
 
-    return psi_xt * del_k
+    return psi_xt
 
-number = int(input("Number of x points:"))
-x_width = int(input("x width on each side of zero :"))
+if input("Enter y/n :") == "y":
+    number = int(input("Number of x points:"))
+    x_width = int(input("x width on each side of zero :"))
+else:
+    number = 900
+    x_width = 300
 
 x_space  = np.linspace(-x_width,x_width,number)
-psi_space = np.zeros(number)
 
-"""plt.plot(k_linspace, amplitude_linspace)
-plt.show()
-"""
-t = 0.1
-while t == 0.1:
-    print(t)
-    for i in range(number):
-        psi_space[i] = psi(x_space[i],t)
 
-    plt.plot(x_space, psi_space)
+#plt.plot(k_linspace, amplitude_linspace)
+#plt.show()
+
+t = 0
+ON = True
+while ON:
+    psi_space = psi(x_space, t)
+
+    plt.plot(x_space, psi_space,label = "At time : {}".format(t), zorder = 1)
+    plt.plot(x_space, np.zeros(number), "k-", label = "x-axis", zorder = 2)
+    plt.scatter([k0 * t],[0],color = "r", s = 100, label = "Classical Partcle", zorder = 3)
+    print([k0 * t],[0])
+    plt.legend()
     plt.show()
 
     t += 10
+    if t >= 30:
+        ON = False
