40ef56079272661f1f0dcc90f74ddecd19cab1df
DebChanda <Not inportant>
2018-09-14 22:58:28 +0530

    Arranged files

---
+++ Assignment 4/energy_finder.py
@@ -0,0 +1,92 @@
+#! /usr/bin/env python3
+
+# Solution of first numerical problem of Assignment 4.
+
+import os
+import time
+
+start = time.time()
+os.system("clear")
+
+
+def func(energy, x, power):
+    dummy = 4 * ((2 * (energy - x**power))**0.5)
+    return dummy
+
+
+def integrate(energy, power):
+    ulim = energy ** (1 / power)
+    llim = 0
+    div = 15000
+    min_div = (ulim - llim) / div
+    x_value = []
+    for i in range(int(div) + 1):
+        x_value.append(llim + i * min_div)
+
+    value = 0
+    for i in range(div):
+        xs = x_value[i]
+        dummy = func(energy, xs, power)
+        value += dummy
+
+    ans = value * min_div
+    # print("For energy ", energy, "value is", ans)
+    return ans
+
+
+def check_integer(value, integer, min_div):
+    if (value >= integer - min_div):
+        if (value <= integer + min_div):
+            return "ok"
+        else:
+            return "large"
+    else:
+        return "less"
+
+
+power = int(input("Enter the power of 'x' in V(x) : "))
+number = int(input("Enter the number of states : "))
+# accuracy = int(input("Enter accuracy, digit after decimal upto which you want the answer to be accurate:"))
+alpha = 2 * power
+beta = 2
+gamma = -power
+
+accuracy = 4
+start_energy = 0.0
+min_div = 10**(-accuracy)
+mod1 = 1
+mod2 = 0.1
+
+print("Energy will be in units of (h^({})k^({})m^({}))^1/{}".format(alpha, beta, gamma, (2 + power)))
+for i in range(1, number + 1):
+    mod = mod1
+    energy_data = [start_energy, start_energy]
+    not_found = True
+    less_value = 0.0
+    large_value = 0.0
+    while(not_found):
+        pxdx = integrate(energy_data[0], power)
+        confirm = check_integer(pxdx, i, min_div)
+        if confirm == "less":
+            # print("less")
+            less_value = energy_data[0]
+            energy_data[0] = less_value + mod
+        elif confirm == "large":
+            # print("large")
+            large_value = energy_data[0]
+            energy_data[0] = less_value + mod * mod2
+            energy_data[1] = large_value
+            mod = mod * mod2
+        else:
+            not_found = False
+            stored_energy = energy_data[0]
+            check = 0
+            while(confirm == "ok"):
+                pxdx = integrate(stored_energy + check * mod, power)
+                confirm = check_integer(pxdx, i, min_div)
+                check += 1
+
+            final_answer = stored_energy + mod * (check - 2)
+            print("The modf-energy for state {} is : {:.{}f}.\n".format(i, final_answer, accuracy))
+
+print("Time taken : {}".format(time.time() - start))
---
+++ Assignment 6/Hermite.py
@@ -0,0 +1,60 @@
+#! /usr/bin/env python3
+
+## Solution of question number 3 of Ass 6.
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+LOWER_X = -5
+UPPER_X = -LOWER_X
+
+def fact(num):
+    dummy = 1
+    factorial = 1
+    while dummy <= num:
+        factorial *= dummy
+        dummy += 1
+    return factorial
+
+
+def hermite_gen(x_value, order):
+
+    h0 = 1
+    h1 = 2 * x_value
+    n = 2
+    hermite_values = [h0, h1]
+#    hermite_values.append(h0,h1)
+    while n <= order:
+        hermite = 2 * x_value * hermite_values[n - 1] - 2 * (n - 1) * hermite_values[n - 2]
+        hermite_values.append(hermite)
+        n += 1
+
+    return hermite_values[order]
+
+
+# order = int(input("Order of Polynomial  : "))
+# print("Answer ", fact(order))
+
+def wavefunc(order):
+
+    x_data = np.linspace(LOWER_X,UPPER_X,1000)
+    psi_data = []
+    const = 1 / (2 ** order * fact(order) * np.pi ** 0.5) ** 0.5
+    for data in x_data:
+        psi = const * np.exp(- data ** 2 / 2) * hermite_gen(data, order)
+        psi_data.append(psi)
+
+    return x_data, psi_data
+
+order = int(input("Order of WaveFunction  : "))
+for x in range(0, order + 1):
+
+    x_data, psi_data = wavefunc(x)
+    plt.plot(x_data, psi_data, label = "WaveFunc at state " + str(x))
+
+potential_data = []
+for data in x_data:
+    potential_data.append(.1 * data ** 2)
+plt.plot(x_data, potential_data, label = "Potential")
+plt.legend()
+plt.show()
